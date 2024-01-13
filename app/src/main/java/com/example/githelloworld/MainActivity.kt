d0d165faca8d7d0fd7ba1d9de7a3db3ff8702a17
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-01-14 01:49:20 +0200

    add listener and toast

--- app/src/main/java/com/example/githelloworld/MainActivity.kt
+++ app/src/main/java/com/example/githelloworld/MainActivity.kt
@@ -1,4 +1,19 @@
 package com.example.githelloworld
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity:Activity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.main_activity)
+        val button:Button = findViewById(R.id.myButton)
+        button.setOnClickListener {
+            val toast = Toast.makeText(this, "button pressed", Toast.LENGTH_LONG)
+            toast.show()
+        }
+    }
 }
