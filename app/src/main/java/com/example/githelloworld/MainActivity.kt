7d46e9d744587a6ce87922e081fbc9843ede2a81
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-01-14 14:27:16 +0200

    my fix 2

--- app/src/main/java/com/example/githelloworld/MainActivity.kt
+++ app/src/main/java/com/example/githelloworld/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.githelloworld
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.Toast
 
