81921ba99ce02dfcfc9d4438cd06bb79004024a8
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-01-14 14:26:11 +0200

    my fix

--- app/src/main/java/com/example/githelloworld/MainActivity.kt
+++ app/src/main/java/com/example/githelloworld/MainActivity.kt
@@ -6,11 +6,11 @@ import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.Toast
 
-class MainActivity:Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        val button:Button = findViewById(R.id.myButton)
+        val button: Button = findViewById(R.id.myButton)
         button.setOnClickListener {
             val toast = Toast.makeText(this, "button pressed", Toast.LENGTH_LONG)
             toast.show()
